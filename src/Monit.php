c7cd1e0229b575bb7e8b1ecfb07e21ee60dc5369
Arkadius Jonczek <arkadius@jonczek.de>
2019-02-06 23:28:27 +0100

    improve Monit class

--- src/Monit.php
+++ src/Monit.php
@@ -10,25 +10,56 @@ namespace Monit;
 class Monit
 {
     /**
-     * Retrieve Monit Status from XML file.
+     * URL of monit status in XML format.
+     *
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * Username for the monit http interface.
+     *
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * Password for the monit http interface.
+     *
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * Monit constructor.
      *
      * @param string $url
      * @param string $username
      * @param string $password
+     */
+    public function __construct($url, $username, $password)
+    {
+        $this->url      = $url;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    /**
+     * Retrieve Monit Status from XML file.
      *
      * @return bool|string
      */
-    protected function getStatusXml($url, $username, $password)
+    public function getStatusXml()
     {
         $context = stream_context_create(
             [
                 'http' => [
-                    'header' => 'Authorization: Basic ' . base64_encode("$username:$password")
+                    'header' => 'Authorization: Basic ' . base64_encode("$this->username:$this->password")
                 ]
             ]
         );
 
-        $xml = file_get_contents($url, false, $context);
+        $xml = file_get_contents($this->url, false, $context);
 
         return $xml;
     }
